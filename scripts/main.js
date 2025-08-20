efc052226dfe948cc8c7587f5f4e0b51cd56ea3a
Scotty Schwartz-Owen <scottyschwartzowen@gmail.com>
2025-08-20 12:21:28 -0700

    Change main.js, add new image

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,10 @@
-// Store a reference to the <h1> in a variable
-const myHeading = document.querySelector('h1');
-// Update the text content of the <h1>
-myHeading.textContent = 'Hello world!';
+const myImage = document.querySelector('img');
+
+myImage.addEventListener('click', () => {
+  const mySrc = myImage.getAttribute('src');
+  if (mySrc === 'images/firefox-icon.png') {
+    myImage.setAttribute('src', 'images/favicon.png');
+  } else {
+    myImage.setAttribute('src', 'images/firefox-icon.png');
+  }
+});
